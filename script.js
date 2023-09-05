152564994e3b9d01f73ded3180afa8302d000cbc
Yaniv Efroni <105995465+yanivef@users.noreply.github.com>
2023-09-05 15:32:46 +0300

    edited comments

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// console.log(game())
 const btnRock = document.querySelector('.rock')
 const btnPaper = document.querySelector('.paper')
 const btnScissors = document.querySelector('.scissors')
@@ -17,9 +16,6 @@ btnScissors.addEventListener('click', (e) => btnHelper(e))
 
 
 
-
-
-
 // Functions 
 
 function getComputerChoice(){
@@ -33,14 +29,7 @@ function getComputerChoice(){
     return 'scissors'
 }
 
-// function getPlayerChoice(){
-//     const playerChoice = prompt('Enter your choice: ')
-//     if(!playerChoice){
-//         console.log('invalid player choice')
-//         return
-//     }
-//     return playerChoice
-// }
+
 
 function btnHelper(e) {
     if(isGameOver()) 
@@ -63,7 +52,7 @@ function isGameOver(){
 
 function playRound(playerSelection){
     const computerSelection = getComputerChoice()
-    console.log(playerSelection)
+
     if(!temp){
         body.appendChild(div)
         div.setAttribute('style', 'display:flex; white-space: pre')   
@@ -104,37 +93,3 @@ function playRound(playerSelection){
             return 'Something went wrong..'
     }
 }
-
-
-// PLAY THE GAME 5 TIMES
-
-// function game(){
-//     let playerScore = 0
-//     let computerScore = 0
-//     let result
-//     for(let i = 1; i <=5; i++) {
-//         if(playerScore === 3 || computerScore === 3)
-//             break    
-        
-//         let playerChoice = getPlayerChoice()
-//         if(!playerChoice)
-//             return
-//         result = playRound(playerChoice, getComputerChoice())
-//         if(result.includes('You Win')){
-//             playerScore++
-//             console.log('Player win round ' + i)
-//         }
-//         else if(result.includes('You Lose')){
-//             computerScore++
-//             console.log('Computer win round ' + i)
-//         }
-//         // ignore case of 'It's a Tie'
-//     }
-
-//     if(playerScore > computerScore)
-//         return 'You Win!'
-//     else if(computerScore > playerScore)
-//         return 'You Lose!'
-//     return 'It\'s a Tie!'
-
-// }
