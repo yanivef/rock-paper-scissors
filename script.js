8ba74d022847d81e3e30b71a3cc90a1c561570bf
Yaniv Efroni <105995465+yanivef@users.noreply.github.com>
2023-09-05 12:15:54 +0300

    added UI

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-console.log(game())
+// console.log(game())
+const btnRock = document.querySelector('.rock')
+const btnPaper = document.querySelector('.paper')
+const btnScissors = document.querySelector('.scissors')
 
 
 
+btnRock.addEventListener('click', () => console.log(playRound('rock')))
+btnPaper.addEventListener('click', () => console.log(playRound('paper')))
+btnScissors.addEventListener('click', () => console.log(playRound('scissors')))
 
 
 // Functions 
@@ -9,26 +15,25 @@ console.log(game())
 function getComputerChoice(){
     const randomNumber = Math.floor(Math.random() * 3) + 1
     if (randomNumber === 1) 
-        return 'Paper' 
+        return 'paper' 
 
     else if (randomNumber === 2)
-        return 'Rock'
+        return 'rock'
 
-    return 'Scissors'
+    return 'scissors'
 }
 
-function getPlayerChoice(){
-    const playerChoice = prompt('Enter your choice: ')
-    if(!playerChoice){
-        console.log('invalid player choice')
-        return
-    }
-    return playerChoice
-}
+// function getPlayerChoice(){
+//     const playerChoice = prompt('Enter your choice: ')
+//     if(!playerChoice){
+//         console.log('invalid player choice')
+//         return
+//     }
+//     return playerChoice
+// }
 
-function playRound(playerSelection, computerSelection){
-    playerSelection = playerSelection.toLowerCase()
-    computerSelection = computerSelection.toLowerCase()
+function playRound(playerSelection){
+    const computerSelection = getComputerChoice()
 
     if (playerSelection === computerSelection)
         return 'It\'s a tie!'
@@ -50,33 +55,36 @@ function playRound(playerSelection, computerSelection){
     }
 }
 
-function game(){
-    let playerScore = 0
-    let computerScore = 0
-    let result
-    for(let i = 1; i <=5; i++) {
-        if(playerScore === 3 || computerScore === 3)
-            break    
+
+// PLAY THE GAME 5 TIMES
+
+// function game(){
+//     let playerScore = 0
+//     let computerScore = 0
+//     let result
+//     for(let i = 1; i <=5; i++) {
+//         if(playerScore === 3 || computerScore === 3)
+//             break    
         
-        let playerChoice = getPlayerChoice()
-        if(!playerChoice)
-            return
-        result = playRound(playerChoice, getComputerChoice())
-        if(result.includes('You Win')){
-            playerScore++
-            console.log('Player win round ' + i)
-        }
-        else if(result.includes('You Lose')){
-            computerScore++
-            console.log('Computer win round ' + i)
-        }
-        // ignore case of 'It's a Tie'
-    }
+//         let playerChoice = getPlayerChoice()
+//         if(!playerChoice)
+//             return
+//         result = playRound(playerChoice, getComputerChoice())
+//         if(result.includes('You Win')){
+//             playerScore++
+//             console.log('Player win round ' + i)
+//         }
+//         else if(result.includes('You Lose')){
+//             computerScore++
+//             console.log('Computer win round ' + i)
+//         }
+//         // ignore case of 'It's a Tie'
+//     }
 
-    if(playerScore > computerScore)
-        return 'You Win!'
-    else if(computerScore > playerScore)
-        return 'You Lose!'
-    return 'It\'s a Tie!'
+//     if(playerScore > computerScore)
+//         return 'You Win!'
+//     else if(computerScore > playerScore)
+//         return 'You Lose!'
+//     return 'It\'s a Tie!'
 
-}
+// }
